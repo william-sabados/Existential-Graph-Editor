440ec04c35dfa822cd20e3026cfb0cec936e3273
PMP9085 <pmp0003@uah.edu>
2016-05-11 14:45:18 -0500

    Add files via upload
    Does NOT work with validator yet, let me know if this is the format you'd like to see for this file

---
+++ ObjectLab/validatorTester.js
@@ -0,0 +1,99 @@
+///// Known correct cases (should pass) //////
+
+str = "()";
+if (validate_input(str) == false)
+	document.write("<BR>FAILED - - - ",str);
+
+str = "(A)";
+if (validate_input(str) == false)
+	document.write("<BR>FAILED - - - ",str);
+
+str = "((A^B)^C)";
+if (validate_input(str) == false)
+	document.write("<BR>FAILED - - - ",str);
+
+str = "(A^(B^C))";
+if (validate_input(str) == false)
+	document.write("<BR>FAILED - - - ",str);
+
+str = "!(!(A^!B)^!C)";
+if (validate_input(str) == false)
+	document.write("<BR>FAILED - - - ",str);
+
+str = "(!A^!(B^!C))";
+if (validate_input(str) == false)
+	document.write("<BR>FAILED - - - ",str);
+
+str = "!(H^(A^B)^C^!(E^!F)^D)";
+if (validate_input(str) == false)
+	document.write("<BR>FAILED - - - ",str);
+
+//////Known incorrect cases (not pass) ///////
+
+str = "A";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "A^";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "^A";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "(A";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "(A(B";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "(A^B^)";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "(A^^B)";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "(!^B)";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "(A^B^C)!";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "())";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "'('A^B)";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "<(>SD^B^D))";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "twelve)";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+str = "!(A)^!(B)^F76^((A))^(673628)";
+if (validate_input(str) == true)
+	document.write("<BR>INCORRECT - - - ",str);
+
+
+
+
+
+
+
+
+
+
+
+
