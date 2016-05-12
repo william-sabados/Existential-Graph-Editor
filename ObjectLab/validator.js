c1a9ce4120819b85c169afdebd72f490cb7cceea
PMP9085 <pmp0003@uah.edu>
2016-05-12 13:50:44 -0500

    Update validator.js
    Update version 4 Changelog:
    - INPUTS: negated letters MUST have the negation within any parends, i.e. (!A), NOT !(A)
    - Added a check for adjacent inputs
    - Fixed "(" and "!(" issue when rebuilding strings
    - Fixed the (A^(B^C)) issue where is any letters between consecutive "(" parends would not rebuild correctly
    - Added true/false statements where needed
    - Fully satisfies/works with the tester file so far 
    - Implemented a global variable that when set to 'true', enables all ERROR and PASS/FAIL output statements; when used for testing/debugging

--- ObjectLab/validator.js
+++ ObjectLab/validator.js
@@ -1,95 +1,160 @@
-
 //(ex) -> ((A^B)^(C^D))
 //	   ->  (A^B) is a component
 
+// *** Change "checker" to true to enable ALL error and pass/fail outputs for testing *** //
+var checker = false ;
+
 function validate_input(str)
 {
-	document.write("<br>",str);
+	//document.write("<br>",str);
 	var tokenized = str.match(/!\(|\w|!\w|\)|\(|\^/gi);		//Tokenized array
-	document.write("<br>",tokenized,"<br>");
+	//document.write("<br>",tokenized,"<br>");
 
 	var arrP = new Array();		//Array of parenthesis (may not be used?)
 	var arrW = new Array();		//Array of everything but parenthesis
 	var letter_count = 0;
 
-	//Checks for any bad input characterSet
+	if (str=="()")
+	{
+		//document.write("<br>PASSED(0) - - -",str);	
+		return true;
+	}
+			
+	for (var x = 0; x < str.length-1; x++)
+	{
+		//Checks for any adjacent characters that are the same (cannot have any)
+		if (str[x] == str[x+1] && ((str[x] != "(" && str[x+1] != "(") && (str[x] != ")" && str[x+1] != ")")))
+		{
+			if (checker) document.write("<br>ERROR::Invalid adjacent inputs");
+			return false;
+		}
+		//Checks for incorrect "!" placements
+		if (str[x].match(/\w|\)/gi) && str[x+1] == "!")
+		{
+			if (checker) document.write("<br>ERROR::Invalid use of not");
+			return false;
+		}
+		
+	}
+	
+	//Checks for any bad input characters
 	if (str.match(/[^\w|\(|\)|\^|!]/gi))
-		document.write("<br>ERROR::Improper syntax");
+	{if (checker) document.write("<br>ERROR::Improper syntax"); 
+	return false;}
 
 	//An egAssertion must be between a pair of "()"
-	else if ((tokenized[0] != "(" || tokenized[0] != "!(") &&(tokenized[tokenized.length-1]!=")"))
-		document.write("<br>ERROR::Assertion must be between parenthesis");
+	else if ((tokenized[0] != "(" && tokenized[0] != "!(") || (tokenized[tokenized.length-1] != ")"))
+	{if (checker) document.write("<br>ERROR::Assertion must be between parenthesis"); 
+	return false;}
 
 	//Checks for the right amount of 'anded' things		
 	else if (str.match(/\^/gi) && str.match(/\w/gi) && (str.match(/\w/gi).length != str.match(/\^/gi).length+1))			
-		document.write("<br>ERROR::Uneven letters or carrots<br>");
+	{if (checker) document.write("<br>ERROR::Uneven letters or carrots<br>"); 
+	return false;}
 
 	//Checks for even amount of braces
 	else if (str.match(/\(|!\(/gi).length != str.match(/\)/gi).length)		
-		document.write("<br>ERROR::Uneven brackets<br>");
+	{if (checker) document.write("<br>ERROR::Uneven brackets<br>"); 
+	return false;}
 
 	//Breaks up the string into indivudual components, and checks each one
-	// ** Does not replace each component after checking **
 	else
 	{
 		for (var x = 0; x < tokenized.length; x++)		
 		{
-			//I don't think this is used anywhere else...
+			//Pushes the INDEX of a given opening brace on to "arrP"
 			if (tokenized[x] == "(" || tokenized[x] == "!(")	
 			{
-				arrP.push(tokenized[x]);
+				arrP.push(x);
 			}
 			else if (tokenized[x] == ")")
 			{
+				var last_openBrace = arrP.pop();
 				//Checks for "^" being the first element inside arrW while checking a multi-component string
-				//  and places a pair of "(" ")" around the component, *it shouldn't have a pair before this point*
+				//  and places a pair of "(" ")" around the component, **it shouldn't have a pair before this point**
 				if (arrW[0] == "^")
 				{
 					arrW.splice(0,1);
 				}
+				if (tokenized[last_openBrace] == "(")
 				arrW.unshift("(");
+				else 
+				arrW.unshift("!(");
 				arrW.push(tokenized[x]);
 				
-				//Concatenates a string to re-create the component
-				var check_string = "";
-				for (var y = 0; y < arrW.length; y++)
-					check_string = check_string.concat(arrW[y]);
+				//Replaces a given module in "tokenized" with a single letter ("X") b/c regex pattern works incorrectly otherwise
+				var temp = tokenized.slice(last_openBrace,x+1);
+				for (var y = 0; y < temp.length; y++)
+				{
+					tokenized.splice(last_openBrace,1);
+				}
+				tokenized.splice(last_openBrace,0,"X");
 				
+				x = x - temp.length;	//Resizes the "for" loop's index to adjust for the change in "tokenized"
+				
+				//Concatenates a string (check_string) to re-create the component/module
+				//	"check_string" is what the regex checks individually for correct format
+				var check_string = "";
+				for (var y = 0; y < temp.length; y++)
+				{
+					check_string = check_string.concat(temp[y]);
+				}
+				
+				//// *** REGEX CHECKING PART *** ////
 				//Regex to check a single letter component, i.e. "(E)"
-				if (check_string.length < 5 && check_string != "()")
+				if ((check_string.length < 5 || (check_string.length == 5 && check_string[0] == "!")) && check_string != "()")
 				{
 					if (check_string.match(/(\w)|(\(\w\))|(!\w)|(!\(\w\))/gi).length == 1)
-						document.write("<br>PASSED(1) - - -",check_string);		
+					{
+						if (checker) document.write("<br>PASSED(1) - - -",check_string);		
+					}
 				}	
 				//Regex to check each component
 				else if (check_string.match(/^((\(|!\()((\w)|(\(\w\))|(!\w)|(!\(\w\)))\^)(((\w)|(\(\w\))|(!\w)|(!\(\w\)))\^)*(((\w)|(\(\w\))|(!\w)|(!\(\w\)))\))$/gi))
 				{
-					document.write("<br>PASSED(2) - - -",check_string);
+					if (checker) document.write("<br>PASSED(2) - - -",check_string);
 				}
-				//Checks the ending case, first / last pair of parends
+				//Checks the ending case, first/last pair of parends
 				else if (check_string == "()" && (x+1) == tokenized.length)
-					document.write("<br>PASSED(3) - - -",check_string);
+				{
+					if (checker) document.write("<br>PASSED(3) - - -",check_string);
+				}
 				else
 				{
-					document.write("<br>FAILED - - -",check_string);
-					//return false;
+					if (checker) document.write("<br>FAILED - - -",check_string);
+					return false;
 				}
-				arrW = [];		//Resets arrW to empty
+				arrW = [];					//Resets arrW to empty
 			}
 			else
 				arrW.push(tokenized[x]);	//Pushes the next letter on to the array
 		}
 	}
-	
 	return true;
 }
 
-str = "((A^B)^(C^D)^(E))";		//Test string
+//Test string
+str = "((A^B)^(C^D)^(E))";		
 //str = "(A^(B^C))";
+str = "!((A^!B)^(!C^D)^E)";
+str = "A";
 
 validate_input(str);
 
+
+
 document.write("<br>----------Validator Executed-------------")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
