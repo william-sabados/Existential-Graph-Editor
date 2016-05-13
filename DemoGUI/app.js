07f258e7187315529fd3e82c4507e4394f68d6e4
J-S-Thomas <jason.simon.thomas@gmail.com>
2016-05-13 15:20:58 -0500

    now renders circle, ellipse, and (P Q) to canvas

--- DemoGUI/app.js
+++ DemoGUI/app.js
@@ -13,3 +13,29 @@ function drawCircle(){
  	var ctx = canvas.getContext("2d");
  	ctx.clearRect(0, 0, canvas.width, canvas.height);
  }
+
+function drawEllipse(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.ellipse(100, 100, 50, 75, 45 * Math.PI/180, 0, 2 * Math.PI);
+    ctx.stroke();
+
+}
+
+function drawP(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.font = "48px serif";
+    ctx.fillText("P", 50, 100);
+}
+
+function drawQ(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.font = "48px serif";
+    ctx.fillText("Q", 50, 100);
+}
