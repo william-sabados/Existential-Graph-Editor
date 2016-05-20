2380c6f060cd80529a3de76e19bfcc3d9763d934
PMP9085 <pmp0003@uah.edu>
2016-05-20 12:37:56 -0500

    Update extras.js

--- ObjectLab/extras.js
+++ ObjectLab/extras.js
@@ -1,14 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////////
 //// NEW FUNCTIONS ////
 
-//searchTerm(object, id) - recursive maybe
+//searchObject(object, id) - recursive maybe
 //-------------------------------------------------------------------------	
-function searchTerm(object, id)
+function searchObject(object, id)
 {
-	for (var x = 0; x < this.terms.length; x++)
+	if (object.id === id) {return object;}
+	for (var x in object)
 	{
-		
+		if (object.hasOwnProperty(x))
+		{
+			var foundID = searchObject(object[x],id);
+			if (foundID) {return foundID;}
+		}
 	}
+	return null;
 }
 	
 //addDoubleNegation(object)
@@ -27,6 +33,7 @@ function addDoubleNegation(object)
 function removeDoubleNegation(object)
 {
 	var str = object.toString();
+	str = str.replace(/\s/g, "");	//REGEX to strip any whitespace
 	str = str.slice(4,str.length-2);
 	var tokenized = str.match(/!\(|\w|!\w|\)|\(/gi);
 	var newEG = parse_Items(tokenized);
@@ -38,7 +45,8 @@ function removeDoubleNegation(object)
 function canRemoveNegation(object)
 {
 	var str = object.toString();
-	//str.replace(/\s/g, '');	REGEX to strip any whitespace
+	str = str.replace(/\s/g, "");	//REGEX to strip any whitespace
+	
 	if (str[0] != "!" && str[1] != "(" && str[str.length-1] != ")")
 		return false;
 	else if (str[2] != "!" && str[3] != "(" && str[str.length-2] != ")")
@@ -46,30 +54,37 @@ function canRemoveNegation(object)
 	else 
 		return true;
 }
+
+var eg1 = new egAssertion("P",true);
+eg1.id = 1;
+document.write("<br>",eg1.id);
+document.write("<br>EG1 stock = ",eg1);
 	
-var eg1 = new egAssertion();
-document.write("<br>EG1 = ",eg1);	
 eg1 = addDoubleNegation(eg1);
-document.write("<br>EG1 = ",eg1);
+document.write("<br>EG1 after add = ",eg1);
+
 eg1 = removeDoubleNegation(eg1);
+document.write("<br>EG1 after rem = ",eg1);
+
+document.write("<br>Can remove? ",canRemoveNegation(eg1));
+
+document.write("<br>");
+eg1 = addDoubleNegation(eg1);
+document.write("<br>EG1 after add = ", eg1);
+
+document.write("<br>Can remove? ",canRemoveNegation(eg1));
+
+document.write("<br>");
+eg1 = new egAssertion("P",true);
+eg1.id = 1;
+eg1 = addDoubleNegation(eg1);
+document.write("<br>Search = ",searchObject(eg1,1));
 document.write("<br>EG1 = ",eg1);
-document.write("<br>Can? ",canRemoveNegation(eg1));
-	
-	
-	
+
+
 document.write("<br>------------Extras Executed--------------")
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	
