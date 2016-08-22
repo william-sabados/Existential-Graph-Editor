f7a0a65f5456e369c03497475bf66b9c9daf540d
Bowen-SW <swb0006@uah.edu>
2016-08-22 08:44:00 -0500

    Resizing now actually works
    I added a button and changed some of the layout

--- App_Example/js/egSetup.js
+++ App_Example/js/egSetup.js
@@ -1,3 +1,4 @@
+// PREVIOUSLY JOINTSCRIPT.JS
 // This line needs to be called after the myholder div tag is declared.
 var graphWidth = (screen.width / 100) * 72.4;
 var paper = new joint.dia.Paper({ el: $('#myholder'), stroke: 1, width: graphWidth, height: 475, gridSize: 1, model: graph });
@@ -26,37 +27,12 @@ paper.on('cell:pointerdown',function(cellView,evt,x,y){
         copy(selection.model,cellView.model);
         copying = false;
     }
-    /*
-    if(copying && selection != cellView){
-        let cellCopy = selection.model.clone({ deep: true });
-        let valid = false;
-        if (cellView.model.prop('isNegated')){
-            valid = true;
-        }
-
-        if(valid){
-            cellCopy.forEach(function(item,index,array){
-                cellCopy[index].prop('egId',controller.incrementId());
-            });
-            if(cellCopy[0].get('parent')) graph.getCell(cellCopy[0].get('parent')).unembed(cellCopy[0]);
-            graph.addCell(cellCopy);
-            cellCopy[0].translate(x-cellCopy[0].prop('position/x'),y-cellCopy[0].prop('position/y'));
-            cellView.model.embed(cellCopy[0]);
-            //cellView.model.fitEmbeds();
-            getTopParent(cellView.model).fitEmbeds({deep: true, padding: 15});
-            //cellCopy[0].toFront({ deep: true });
-        }else{
-            alert('Not a valid place to copy to.');
-        }
-        copying = false;
-    }*/
-
-    // If there's already something selected, unhighlight it
+    
+    // Unhighlight everything
     allCells = graph.getCells();
     allCells.forEach(function(item,index,array){
         paper.findViewByModel(allCells[index]).unhighlight();
     });
-    //if(selection) selection.unhighlight();
     // Select the cell the user is clicking on
     selection = cellView;
     //Bring cell to front
@@ -67,22 +43,27 @@ paper.on('cell:pointerdown',function(cellView,evt,x,y){
     prevMouseY = y;
 });
 
-/* Resizing, but need to figure out how to prevent position change (drag to resize and to move, at the moment)
+paper.on('cell:pointerup',function(cellView,evt,x,y){
+    resizing = false;
+});
+
+/* Resizing */
 paper.on('cell:pointermove',function(cellView,evt,x,y){
-    if(cellView == selection){
+    if(cellView == selection && resizing){
         if(cellView.model.prop('size/width') + (x-prevMouseX) > 0 && cellView.model.prop('size/height') + (y-prevMouseY) > 0)
         cellView.model.resize(cellView.model.prop('size/width') + (x-prevMouseX),cellView.model.prop('size/height') + (y-prevMouseY));
+        cellView.model.translate(-x+prevMouseX,-y+prevMouseY);
         prevMouseX = x;
         prevMouseY = y;
-        resizing = true;
     }
 });
-*/
+
 
 // On click in a blank area
 paper.on('blank:pointerdown',function(evt,x,y){
     if(selection) selection.unhighlight();
     selection = null;
+    resizing = false;
 });
 
 removeSelection = function(){
@@ -90,4 +71,5 @@ removeSelection = function(){
         selection.unhighlight();
         selection = null;
     }
+    resizing = false;
 };
